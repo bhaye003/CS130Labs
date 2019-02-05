e5253596d3661b9abb7845c4d343cd299a00750b
Bradley Hayes <bhaye003@bolt.cs.ucr.edu>
2019-02-04 16:48:28 -0800

    finished checkpoint 2

--- grading/phong_shader.cpp
+++ grading/phong_shader.cpp
@@ -8,8 +8,23 @@ vec3 Phong_Shader::
 Shade_Surface(const Ray& ray,const vec3& intersection_point,
     const vec3& normal,int recursion_depth) const
 {
-    vec3 color;
-    vec3 ambient = world.ambient_color * world.ambient_intensity * color_ambient;
-    color = ambient;
-    return color;
+	vec3 color;
+	vec3 ambient = ambient + color_ambient * world.ambient_intensity * world.ambient_color;
+	vec3 differ;
+	vec3 specular;	
+	Ray shadowray;
+	Light *light_ptr;
+   
+	for(unsigned i = 0; i < world.lights.size(); ++i){
+		light_ptr = world.lights.at(i);
+		double diffMax = std::max(dot(normal.normalized(), -(intersection_point - (light_ptr -> position)).normalized()), 0.0);
+		vec3 L = light_ptr -> position - intersection_point;
+		double specMax = pow(std::max(dot((intersection_point - ray.endpoint).normalized(), (L - 2 * dot(L, normal)*normal).normalized() ), 0.0), specular_power);		  differ = differ + color_diffuse * light_ptr -> Emitted_Light(intersection_point - light_ptr -> position) * diffMax;
+		specular = specular + color_specular * light_ptr -> Emitted_Light(intersection_point - light_ptr -> position) * specMax;
+	}
+		
+		
+
+	color = ambient + differ + specular;
+	return color;
 }
--- grading/sphere.cpp
+++ grading/sphere.cpp
@@ -4,31 +4,40 @@
 // Determine if the ray intersects with the sphere
 Hit Sphere::Intersection(const Ray& ray, int part) const
 {
-    double a  = 1;
-    double b = 2*dot( ray.direction, ray.endpoint - center);
-    double c = dot(ray.endpoint - center, ray.endpoint - center) - radius*radius;
+
+    vec3 vec = ray.endpoint - this -> center;
+    vec3 u = ray.direction;
+    	
+    double a  = dot(u,u);
+    double b = 2*dot(vec, u);
+    double c = dot(vec, vec) - pow(this -> radius, 2);
     double dis = b*b - 4*a*c;
-    double t = 0, t0 = 0, t1 = 0;
+    double t1, t2;
     if( dis == 0){
-	t = 0.5 * -b / a;
+	t1 = 0.5 * -b / a;
+	t2 = 0;
+	
+	if(t1 >= small_t){
+		return{this, t1, part};
+	}
     } 
     else if( dis > 0){
-	t0 = 0.5 * (-b + sqrt(dis)) / a;
 	t1 = 0.5 * (-b + sqrt(dis)) / a;
-	t = (t0 >= t1)  ? t0 : t1;
+	t2 = 0.5 * (-b - sqrt(dis)) / a;
+	if(t1 >= small_t && t1< t2){
+		return{this, t1, part};
+	}
+	else if(t2 >= small_t && t2 < t1){
+		return{this, t2, part};
+	}
     }
-    if(t >= small_t) {
-	return{this, t, part};
-    }
-
-
-    return {0,0,0};
+    return {nullptr,0,part};
 }
 
 vec3 Sphere::Normal(const vec3& point, int part) const
 {
     vec3 normal;
-    normal = (point - center).normalized();
+    normal = (point - center)/radius;
     return normal;
 }
 
